80500dfc49f6ee1b2ae6ee1db3bb6cfe88a8348f
Marlow14 <c_a_marlow14@yahoo.com>
2016-07-28 16:58:31 -0400

    today's work

---
+++ src/sample/ProgramDriver.java
@@ -0,0 +1,63 @@
+package sample;
+
+public class ProgramDriver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+//		int x = 0;
+//		int[] myArray = new int [3];
+//		myArray[0] = 11;
+//		myArray[1] = 20;
+//		myArray[2] = 30;
+//		
+//		System.out.println(myArray[x]);
+//		System.out.println(myArray[x++]);
+//		System.out.println(myArray[++x]);
+		
+//		double x = 3;
+//		double ans = 0;
+//		x *= 10;
+//		ans = x;
+//		
+//		System.out.println(ans);
+		
+//		int y = 0;
+//		while(y<5)
+//		{
+//			System.out.println(y);
+//			y++;
+//		}
+//		
+//		for (int x=0;x<5;x++)
+//			System.out.println(x);
+//		
+//		int z = 0;
+//		do
+//		{
+//			System.out.println(z);
+//			z++;
+//		}while (z<5);
+//		
+//		int w = 0;
+//		for (int i=0;i<5&&w<4;i++){
+//			System.out.println(i);
+//			w = w + 1;
+//		}
+//		
+//		
+//		for(int u=0, i=0;i<5&&u<4;i++,u = u + 1){
+//			System.out.println(i);
+//		}
+		
+		for (int v=1; v<=12; v++){
+			for(int p=1; p<=12; p++){
+				System.out.print(v*p + "\t ");
+			}
+						System.out.println("");
+		}
+		
+		
+	}
+
+}
